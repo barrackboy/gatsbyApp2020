4fc4895875718846fbf6d5165ad40229bc3b8745
Collins <shout2me@gmail.com>
2020-11-13 18:57:36 +0200

    Stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
+// scope css locally
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
-}
+import Layout from "../components/layout"
+import { ExampleButton } from "../components/button"
+export default () => (
+  <Layout>
+    <h1 style={{ color: "green", textTransform: "uppercase" }}>
+      hello from gatsby
+    </h1>
+    <h1>hello world</h1>
+    <h1>hello people</h1>
+    <ExampleButton>click me</ExampleButton>
+  </Layout>
+)
